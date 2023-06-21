b1f279c9fe502ee92fbb66f8748bdcaaea6d2f23
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-06-21 11:41:47 -0300

    Update

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,11 +11,37 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "5D52932F-828A-41E2-8B8F-ADA770ECF705"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
         
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let currency = parseJson(currencyData: safeData){
+                        print("OK")
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    func parseJson(currencyData: Data) {
+        return "OK"
     }
 }
---
+++ ByteCoin/Model/CurrencyModel.swift
@@ -0,0 +1,9 @@
+//
+//  CurrencyModel.swift
+//  ByteCoin
+//
+//  Created by Orlando Moraes Martins on 21/06/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
